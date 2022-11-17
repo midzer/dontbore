90a56d490de237b5617523283371b6208e87ba11
midzer <midzer@gmail.com>
2022-11-17 10:14:18 +0100

    fix wording

--- src/lib/Faq/index.svelte
+++ src/lib/Faq/index.svelte
@@ -9,7 +9,7 @@
     <p>It is always good to decentralize the web. In addition, it seems BugMeNot is not maintained any more (no HTTPS for example).</p>
   </details>
   <details>
-    <summary>How to create a fake account?</summary>
+    <summary>How do I create a fake account?</summary>
     <p>There are multiple throw away or trash eMail services around. Your search engine is your friend.</p>
   </details>
 </article>
