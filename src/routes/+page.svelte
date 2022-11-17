a4b2dec1e90ebbf5addf9cde050c861b93ad2a49
midzer <midzer@gmail.com>
2022-11-17 11:09:03 +0100

    add arrow to text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <h1>Find and share logins</h1>
-<p>Enter a website above to check if the DontBore community has shared any logins for it.</p>
+<p>Enter a website above ↑ to check if the DontBore community has shared any logins for it.</p>
 
 <Faq />
