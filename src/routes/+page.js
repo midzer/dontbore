2020ff6fbdbc71297bee57e778602e8d649bfdec
midzer <midzer@gmail.com>
2022-11-19 19:08:56 +0100

    simplify endpoints

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,6 +1,6 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
-    const res = await fetch(`https://api.dontbo.re/recent`);
+    const res = await fetch(`https://api.dontbo.re/`);
     const domains = await res.json();
    
     return { domains };
--- src/routes/[domain]/+page.js
+++ src/routes/[domain]/+page.js
@@ -1,6 +1,6 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch, params }) {
-    const res = await fetch(`https://api.dontbo.re/${params.domain}/logins`);
+    const res = await fetch(`https://api.dontbo.re/${params.domain}`);
     const logins = await res.json();
    
     return { logins };
