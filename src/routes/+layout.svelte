0ada0a2dc512095963e3d9b954fbd4cdaf888fd8
midzer <midzer@gmail.com>
2022-11-23 18:49:52 +0100

    add favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,13 @@
 <svelte:head>
   <title>DontBore{ domain ? ' - ' + domain : ''}</title>
   <meta name="description" content="Find and share logins provided by the community">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
   <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#07080d">
+  <meta name="msapplication-TileColor" content="#07080d">
+  <meta name="theme-color" content="#07080d">
 </svelte:head>
 
 <Header />
